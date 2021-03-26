68cb2825ca9a9f9d4c421bdce80cfdb2f0d48f66
Brody Rethy <brodyrethy@gmail.com>
2021-03-26 19:32:49 -0300

    pushing

--- recycle-bin.sh
+++ recycle-bin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #	By: Brody Rethy
-#	Website: https://rethy.xyz 
+#	Website: https://rethy.xyz
 #
 #	Name: rethyxyz_recycle_bin.sh
 #	Version: 1.0
@@ -13,21 +13,21 @@
 #
 
 ##
-## TODO: Implement -h arg to prompt usage aid
+## TODO Implement -h arg to prompt usage aid
 ## TODO function for remove file
 ## TODO function for move file to trash
-# TODO Check age of first file/dir in Trash dir. if older than 30 days, remove.
+## TODO Check age of first file/dir in Trash dir. if older than 30 days, remove.
 ##
 
 remove_file() {
-		{ /usr/bin/rm -rf "$FILE" > /dev/null 2>&1 && echo "Removed file $FILE" ; } || { /usr/bin/rm -rf "$FILE" > /dev/null 2>&1 && echo "Removed file $FILE using sudo" ; }
+	{ /usr/bin/rm -rf "$FILE" > /dev/null 2>&1 && echo "Removed file $FILE" ; } || { /usr/bin/rm -rf "$FILE" > /dev/null 2>&1 && echo "Removed file $FILE using sudo" ; }
 }
 
 move_to_trash_bin() {
 	{ mv -iv "$FILE" "$TRASH_DIR" > /dev/null 2>&1 && echo "$FILE to $TRASH_DIR"; } || { /usr/bin/sudo mv -iv "$FILE" "$TRASH_DIR" > /dev/null 2>&1 && echo "$FILE to $TRASH_DIR using sudo"; }
 }
 
-large_file() {
+handle_large_file() {
 	echo "$FILE size is $FILE_SIZE, over 20GB, and too large for the recycle bin"
 	echo "Do you still want to remove it (this is permanent)? (y/n)"
 
@@ -52,12 +52,15 @@ handle_file_exists() {
 		FILE_NUM=$((FILE_NUM+1))
 		NEW_FILE="${FILE_NUM}_${FILE}"
 	done
-	{ mv "$FILE" "$NEW_FILE" > /dev/null 2>&1 && FILE=$NEW_FILE; } || { /usr/bin/sudo mv "$FILE" "$NEW_FILE" > /dev/null 2>&1 && FILE=$NEW_FILE; }
+
+	mv "$FILE" "$NEW_FILE" > /dev/null 2>&1 || /usr/bin/sudo mv "$FILE" "$NEW_FILE" > /dev/null 2>&1
+
+	FILE=$NEW_FILE
 }
 
 TRASH_DIR="$HOME/.Trash/files/"
 
-# Check if there are args given
+# IF ARGS GIVEN
 if [[ $# -eq 0 ]]
 then
 	echo ":: No filename(s) given"; exit 1
@@ -65,7 +68,7 @@ fi
 
 FILES=( "$@" )
 
-# check if ~/.Trash/files doesn't exists
+# IF $TRASH_DIR ! EXISTS
 if [[ ! -d "$TRASH_DIR" ]]
 then
 	mkdir -p "$TRASH_DIR"
@@ -73,21 +76,19 @@ fi
 
 # Start main program loop
 #
-# The for loops loops through all args given,
-# which should I'm assuming are files.
+# The for loops loops through all args given, which should I'm assuming are
+# files.
 for FILE in "${FILES[@]}"
 do
-
 	# CHECK IF FILE EXISTS
 	if [[ -e "$FILE" ]]
 	then
-
-		# CHECK IF EMPTY FOR LINK
+		# CHECK IF LINK || EMPTY
 		if [[ -L "$FILE" ]] || [[ ! -s "$FILE" ]]
 		then
 			remove_file
 		else
-			# GET SIZE OF FILE
+			# GET FILE SIZE
 			FILE_SIZE=$(du -h -s -m "$FILE" | awk '{print $1}')
 
 			# CHECK IF OVER 20GB
@@ -98,18 +99,14 @@ do
 
 			if [[ $FILE_SIZE -ge 20000 ]]
 			then
-				large_file
+				handle_large_file
 			else
-				# Move file to ~/.Trash/files
 				move_to_trash_bin
 			fi
 		fi
-
 	else
 		echo ":: File given does not exist"
 	fi
-
 done
 
-# A successful exit, if it gets here
 exit 0
