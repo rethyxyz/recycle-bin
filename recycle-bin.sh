e1edae2f47da715327ca60ba467af00df5e9ccea
Brody Rethy <brodyrethy@gmail.com>
2021-03-26 19:01:54 -0300

    final updated for a while, all refactored

--- recycle-bin.sh
+++ recycle-bin.sh
@@ -11,8 +11,49 @@
 #   I still need to implement some features, such
 #	as file compression, but I'll work on it in time.
 #
-# TODO: Implement -h arg to prompt usage aid
-#
+
+##
+## TODO: Implement -h arg to prompt usage aid
+## TODO function for remove file
+## TODO function for move file to trash
+# TODO Check age of first file/dir in Trash dir. if older than 30 days, remove.
+##
+
+remove_file() {
+		{ /usr/bin/rm -rf "$FILE" > /dev/null 2>&1 && echo "Removed file $FILE" ; } || { /usr/bin/rm -rf "$FILE" > /dev/null 2>&1 && echo "Removed file $FILE using sudo" ; }
+}
+
+move_to_trash_bin() {
+	{ mv -iv "$FILE" "$TRASH_DIR" > /dev/null 2>&1 && echo "$FILE to $TRASH_DIR"; } || { /usr/bin/sudo mv -iv "$FILE" "$TRASH_DIR" > /dev/null 2>&1 && echo "$FILE to $TRASH_DIR using sudo"; }
+}
+
+large_file() {
+	echo "$FILE size is $FILE_SIZE, over 20GB, and too large for the recycle bin"
+	echo "Do you still want to remove it (this is permanent)? (y/n)"
+
+	while true
+	do
+		read CHOICE
+
+		case "$CHOICE" in
+			y | Y) { /usr/bin/rm -rf "$FILE" > /dev/null 2>&1 && echo "Removed $FILE of size $FILE_SIZE"; } || { /usr/bin/sudo /usr/bin/rm -rf "$FILE" > /dev/null 2>&1 && echo "Removed $FILE of size $FILE_SIZE using sudo"; } ;;
+			n | N) echo "Exit, file not removed." ;;
+			*) echo ":: Choice not recognized, try again"
+		esac
+	done
+}
+
+handle_file_exists() {
+	FILE_NUM=0
+	NEW_FILE=$FILE
+
+	while [[ -e "$TRASH_DIR/$NEW_FILE" ]]
+	do
+		FILE_NUM=$((FILE_NUM+1))
+		NEW_FILE="${FILE_NUM}_${FILE}"
+	done
+	{ mv "$FILE" "$NEW_FILE" > /dev/null 2>&1 && FILE=$NEW_FILE; } || { /usr/bin/sudo mv "$FILE" "$NEW_FILE" > /dev/null 2>&1 && FILE=$NEW_FILE; }
+}
 
 TRASH_DIR="$HOME/.Trash/files/"
 
@@ -25,7 +66,7 @@ fi
 FILES=( "$@" )
 
 # check if ~/.Trash/files doesn't exists
-if [[ ! -d $TRASH_DIR ]]
+if [[ ! -d "$TRASH_DIR" ]]
 then
 	mkdir -p "$TRASH_DIR"
 fi
@@ -36,87 +77,38 @@ fi
 # which should I'm assuming are files.
 for FILE in "${FILES[@]}"
 do
-	# Check if symlink
-	#
-	# Remove file if link
-	if [[ -L "$FILE" ]]
+
+	# CHECK IF FILE EXISTS
+	if [[ -e "$FILE" ]]
 	then
-		/usr/bin/rm "$FILE" && echo "Removed symlink $FILE" || echo ":: Failed to remove symlink $FILE"
-	   	exit 1
-	fi
 
-	# Check if file given exists
-	if [[ ! -e "$FILE" ]]
-	then
-		echo ":: File given does not exist"; exit 1
-	fi
-
-	# Check if file empty
-	if [[ ! -s "$FILE" ]]
-	then
-		/usr/bin/rm -rf "$FILE" || /usr/bin/sudo /usr/bin/rm -rf "$FILE"
-		echo "Removed empty file $FILE"
-		exit 1
-	fi
-
-	FILE_NUM=0
-
-	# Check age of first file/dir in Trash dir
-	# if older than 30 days, remove
-	# TODO
-
-	# check size of file in megabytes
-	SIZE=$(du -h -s -m "$FILE" | awk '{print $1}')
-
-	# This is somewhat of a hack job
-	if [[ $SIZE -ge 20000 ]]
-	then
-		echo ":: File is $SIZE, over 20GB, and is too large for the recycle bin"
-		echo "Do you still want to remove it (this is permanent)? (y/n)"
-
-		while true
-		do
-			read CHOICE
-
-			case "$CHOICE" in
-				y)
-					/usr/bin/rm -rf "$FILE" || /usr/bin/sudo /usr/bin/rm -rf "$FILE" 
-					exit 1
-					;;
-				n)
-					echo ":: Exit, file not removed"
-					exit 0
-					;;
-				*) echo ":: Choice not recognized, try again"
-			esac
-		done
-	fi
-
-	# Check if file/dir already exists in .Trash/files dir
-	#
-	# I know that there is an option built into mv
-	# to prompt if I want to override, but I think this is safer,
-	# as you can't accidentally accept to overriding important files.
-	NEW_FILE=$FILE
-
-	while true
-	do
-		if [[ -e "$TRASH_DIR/$NEW_FILE" ]]
+		# CHECK IF EMPTY FOR LINK
+		if [[ -L "$FILE" ]] || [[ ! -s "$FILE" ]]
 		then
-			FILE_NUM=$((FILE_NUM+1))
-			NEW_FILE="${FILE_NUM}_${FILE}"
+			remove_file
 		else
-			{ mv "$FILE" "$NEW_FILE"; FILE=$NEW_FILE; } > /dev/null 2>&1
-			break
+			# GET SIZE OF FILE
+			FILE_SIZE=$(du -h -s -m "$FILE" | awk '{print $1}')
+
+			# CHECK IF OVER 20GB
+			if [[ -e "$TRASH_DIR/$NEW_FILE" ]]
+			then
+				handle_file_exists
+			fi
+
+			if [[ $FILE_SIZE -ge 20000 ]]
+			then
+				large_file
+			else
+				# Move file to ~/.Trash/files
+				move_to_trash_bin
+			fi
 		fi
-	done
 
-	#TODO: Check if file needs root
+	else
+		echo ":: File given does not exist"
+	fi
 
-	# Move file to ~/.Trash/files
-	#
-	# TODO: Change stdout to something better
-	mv -iv "$FILE" $TRASH_DIR > /dev/null 2>&1 && echo "$FILE to $TRASH_DIR" || echo ":: Failed to move $FILE to $TRASH_DIR"
 done
 
 # A successful exit, if it gets here
