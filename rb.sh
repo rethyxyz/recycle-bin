53d64ddfb6afa89203bed64e823b583631b82b1f
Brody Rethy <brodyrethy@gmail.com>
2021-05-06 18:06:45 -0300

    Code clean up. Status messages have more depth.

--- rb.sh
+++ rb.sh
@@ -6,93 +6,62 @@
 # Name: rb.sh
 #
 # Summary:
-# A script to imitate the Windows recycle bin. I still need
-# to implement some features, such as file compression, but
-# I'll work on it in time.
+# A simple script to imitate the Windows recycle bin on Unix. I still need to
+# implement some features, such as compression, but I'll work on it in time.
 
-display_help() {
-	echo "rb.sh [FILE] [FILE 2] [FILE 3] ..."
-	echo ""
-	echo "List the file/files or directory/directories you want to remove."
-}
-
-file_remove() {
-	{ /usr/bin/rm -rf "$FILE" && echo "$TEXT"; } || { /usr/bin/sudo /usr/bin/rm -rf "$FILE" && echo "$TEXT" using sudo; }
-}
-
-file_to_trash() {
-	/usr/bin/mv "$FILE" "$RECYCLE_DIR/$FILE" && echo "$FILE to $RECYCLE_DIR"
-}
-
-handle_file_exists() {
-	FILE_NUM=0
-	NEW_FILE=$FILE
-
-	while [[ -e "$RECYCLE_DIR/$NEW_FILE" ]]
-	do
-		FILE_NUM=$((FILE_NUM+1))
-		NEW_FILE="${FILE_NUM}_${FILE}"
-	done
-
-	/usr/bin/mv "$FILE" "$NEW_FILE" || /usr/bin/sudo /usr/bin/mv "$FILE" "$NEW_FILE"
-
-	FILE=$NEW_FILE
-}
-
+ITEMS=("$@")
 RECYCLE_DIR="$HOME/.Trash"
 
-# IF ARGS GIVEN
-if [[ $# -eq 0 ]]
-then
-	echo ":: No filename(s) given"
-	echo "::"
-	echo ":: Use --help/-h for help"
-	exit 1
-elif [[ $@ = "--help" ]] || [[ $@ = "-h" ]]
-then
-	display_help
-	exit 0
-fi
+display_help() {
+    echo "rb.sh [ITEM] [ITEM 2] [ITEM 3] ..."
+    echo ""
+    echo "List the item(s) you want to remove."
+}
 
-FILES=( "$@" )
+handle_item_exists() {
+    ITEM_PREFIX=0
+    NEW_ITEM=$ITEM
 
-# IF $RECYCLE_DIR ! EXISTS
-if [[ ! -d "$RECYCLE_DIR" ]]
-then
-	mkdir -p "$RECYCLE_DIR"
-fi
+    while [ -e "$RECYCLE_DIR/$NEW_ITEM" ]; do
+        ITEM_PREFIX=$((ITEM_PREFIX+1))
+        NEW_ITEM="${ITEM_PREFIX}_${ITEM}"
+    done
 
-# Start main program loop
-#
-# Loops through all args given, which should I'm assuming are files. They don't
-# need to be, but I'm using it as the variable name none the less.
-for FILE in "${FILES[@]}"
-do
-	# CHECK IF FILE EXISTS
-	if [[ -e "$FILE" ]]
-	then
-		# IF $LINK
-		if [[ -L "$FILE" ]]
-		then
-			TEXT="Removed link $FILE"; file_remove $TEXT
-		# IF empty
-		elif [[ ! -s "$FILE" ]]
-		then
-			TEXT="Removed empty file $FILE"; file_remove $TEXT
-		else
-			# CHECK IF FILE EXISTS IN RECYCLE BIN
-			if [[ -e "$RECYCLE_DIR/$FILE" ]]
-			then
-				handle_file_exists
-			fi
+    /usr/bin/mv "$ITEM" "$NEW_ITEM" || /usr/bin/sudo /usr/bin/mv "$ITEM" "$NEW_ITEM"
 
-			file_to_trash
-		fi
-	else
-		echo ":: File given does not exist"
-		echo "::"
-		echo ":: Use --help/-h for help"
-	fi
+    ITEM=$NEW_ITEM
+}
+
+# If not args given, echo and exit.
+if [ $# -eq 0 ]; then echo -e ":: No filename(s) given\n::"; display_help; exit 1
+# Else if an arg matches, call display_help.
+elif [ "$*" = "--help" ] || [ "$*" = "-h" ]; then display_help; exit 0; fi
+
+# If $RECYCLE_DIR doesn't exists, create it.
+if [ ! -d "$RECYCLE_DIR" ]; then mkdir -p "$RECYCLE_DIR"; fi
+
+# Start main program loop. Loops through all args given.
+for ITEM in "${ITEMS[@]}"; do
+    # If item exists, continute.
+    if [ -e "$ITEM" ]; then
+        # If link, remove.
+        if [ -L "$ITEM" ]; then
+            { /usr/bin/rm -rf "$ITEM" && echo "Removed link \"$ITEM\"."; } || { /usr/bin/sudo /usr/bin/rm -rf "$ITEM" && echo "Removed link \"$ITEM\" using sudo."; }
+        # Else if item empty, remove.
+        elif [ ! -s "$ITEM" ]; then
+            TEXT="Removed empty file $ITEM"
+            { /usr/bin/rm -rf "$ITEM" && echo "$TEXT"; } || { /usr/bin/sudo /usr/bin/rm -rf "$ITEM" && echo "$TEXT" using sudo; }
+        else
+            # If item exists in recycle bin, rename until no match.
+            if [ -e "$RECYCLE_DIR/$ITEM" ]; then handle_item_exists; fi
+            # Move file to trash
+            /usr/bin/mv "$ITEM" "$RECYCLE_DIR/$ITEM" && echo "Item \"$ITEM\" to \"$RECYCLE_DIR\"" || echo "Failed to move "
+        fi
+    else
+        echo ":: Item \"$ITEM\" does not exist."
+        echo "::"
+        echo ":: Use --help/-h for help."
+    fi
 done
 
 exit 0
