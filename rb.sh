26e430009be9217192777e6701f4c6eafc8d0996
Brody Rethy <brodyrethy@gmail.com>
2021-06-01 21:26:51 -0300

    Now POSIX shell compliant, and rm'ed rb.py.

--- rb.sh
+++ rb.sh
@@ -9,14 +9,22 @@
 # A simple script to imitate the Windows recycle bin on Unix. I still need to
 # implement some features, such as compression, but I'll work on it in time.
 
-ITEMS=("$@")
+#############
+# VARIABLES #
+#############
+
 # This can be changed to anything.
 RECYCLE_DIR="$HOME/.Trash"
 
+
+
+#############
+# FUNCTIONS #
+#############
+
 display_help() {
-    echo "rb.sh [ITEM] [ITEM 2] [ITEM 3] ..."
-    echo ""
-    echo "List the item(s) you want to remove."
+    printf "rb.sh [ITEM] [ITEM 2] [ITEM 3] ...\n\n"
+    printf "List the item(s) you want to remove.\n"
 }
 
 handle_item_exists() {
@@ -33,34 +41,69 @@ handle_item_exists() {
     ITEM=$NEW_ITEM
 }
 
-# If not args given, echo and exit.
-if [ $# -eq 0 ]; then echo -e ":: No filename(s) given\n::"; display_help; exit 1
+
+
+########
+# MAIN #
+########
+
+# If not args given, printf and exit.
+if [ $# -eq 0 ]; then
+    printf ":: No filename(s) given\n::"
+    display_help
+    exit 1
 # Else if an arg matches, call display_help.
-elif [ "$*" = "--help" ] || [ "$*" = "-h" ]; then display_help; exit 0; fi
+elif [ "$*" = "--help" ] || [ "$*" = "-h" ]; then
+    display_help
+    exit 0
+fi
 
 # If $RECYCLE_DIR doesn't exists, create it.
-if [ ! -d "$RECYCLE_DIR" ]; then mkdir -p "$RECYCLE_DIR"; fi
+if [ ! -d "$RECYCLE_DIR" ]; then
+    mkdir -p "$RECYCLE_DIR"
+fi
 
 # Start main program loop. Loops through all args given.
-for ITEM in "${ITEMS[@]}"; do
+for ITEM in "$@"; do
     # If item exists, continute.
     if [ -e "$ITEM" ]; then
         # If link, remove.
         if [ -L "$ITEM" ]; then
-            { /usr/bin/rm -rf "$ITEM" && echo "Removed link \"$ITEM\"."; } || { /usr/bin/sudo /usr/bin/rm -rf "$ITEM" && echo "Removed link \"$ITEM\" using sudo."; }
-        # Else if item empty, remove.
+            {
+                /usr/bin/rm -rf "$ITEM" \
+                && printf "Removed link \"%s\"\n." "$ITEM"
+            } || {
+                /usr/bin/sudo /usr/bin/rm -rf "$ITEM" \
+                && printf "Removed link \"%s\" using sudo.\n" "$ITEM"
+            }
+        # If item empty, remove.
         elif [ ! -s "$ITEM" ]; then
-            { /usr/bin/rm -rf "$ITEM" && echo "Removed empty file \"$ITEM\"."; } || { /usr/bin/sudo /usr/bin/rm -rf "$ITEM" && echo "Removed empty file \"$ITEM\" using sudo."; }
+            {
+                /usr/bin/rm -rf "$ITEM" \
+                && printf "Removed empty file \"%s\".\n" "$ITEM"
+            } || {
+                /usr/bin/sudo /usr/bin/rm -rf "$ITEM" \
+                && printf "Removed empty file \"%s\" using sudo.\n" "$ITEM"
+            }
         else
             # If item exists in recycle bin, rename until no match.
-            if [ -e "$RECYCLE_DIR/$ITEM" ]; then handle_item_exists; fi
+            if [ -e "$RECYCLE_DIR/$ITEM" ]; then
+                handle_item_exists
+            fi
+
             # Move file to trash.
-            { /usr/bin/mv "$ITEM" "$RECYCLE_DIR/$ITEM" && echo "Item \"$ITEM\" to \"$RECYCLE_DIR\""; } || { /usr/bin/sudo /usr/bin/mv "$ITEM" "$RECYCLE_DIR/$ITEM" && echo "Item \"$ITEM\" to \"$RECYCLE_DIR\" using sudo"; }
+            {
+                /usr/bin/mv "$ITEM" "$RECYCLE_DIR/$ITEM" \
+                && printf "Item \"%s\" to \"%s\"\n" "$ITEM" "$RECYCLE_DIR"
+            } || {
+                /usr/bin/sudo /usr/bin/mv "$ITEM" "$RECYCLE_DIR/$ITEM" \
+                && printf "Item \"%s\" to \"%s\" using sudo\n" "$ITEM" "$RECYCLE_DIR"
+            }
         fi
     else
-        echo ":: Item \"$ITEM\" does not exist."
-        echo "::"
-        echo ":: Use --help/-h for help."
+        printf ":: Item \"%s\" does not exist.\n" "$ITEM"
+        printf "::\n"
+        printf ":: Use --help/-h for help.\n"
     fi
 done
 
