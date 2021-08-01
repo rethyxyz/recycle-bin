49db8e3ef8e6a240fddb64f1cda0aeeeaab12bc8
Brody Rethy <brodyrethy@gmail.com>
2021-08-01 17:43:04 -0300

    No longer FORTRAN style.

--- rb.sh
+++ rb.sh
@@ -15,7 +15,7 @@
 #############
 
 # This can be changed to anything.
-RECYCLE_DIR="$HOME/.Trash"
+recycle_dir="$HOME/.Trash"
 
 
 
@@ -24,23 +24,23 @@ RECYCLE_DIR="$HOME/.Trash"
 #############
 
 display_help() {
-    printf "%s [ITEM] [ITEM 2] [ITEM 3] ...\n\n" "$0"
+    printf "%s [item] [item 2] [item 3] ...\n\n" "$0"
     printf "List the item(s) you want to remove.\n"
 }
 
 handle_item_exists() {
-    ITEM_PREFIX=0
-    NEW_ITEM=$ITEM
+    item_prefix=0
+    new_item=$item
 
-    while [ -e "$RECYCLE_DIR/$NEW_ITEM" ]; do
-        ITEM_PREFIX=$((ITEM_PREFIX+1))
-        NEW_ITEM="${ITEM_PREFIX}_${ITEM}"
+    while [ -e "$recycle_dir/$new_item" ]; do
+        item_prefix=$((item_prefix+1))
+        new_item="${item_prefix}_${item}"
     done
 
-    /usr/bin/mv "$ITEM" "$NEW_ITEM" \
-        || /usr/bin/sudo /usr/bin/mv "$ITEM" "$NEW_ITEM"
+    /usr/bin/mv "$item" "$new_item" \
+        || /usr/bin/sudo /usr/bin/mv "$item" "$new_item"
 
-    ITEM="$NEW_ITEM"
+    item="$new_item"
 }
 
 
@@ -59,48 +59,48 @@ if [ "$*" = "--help" ] || [ "$*" = "-h" ]; then
     exit 0
 fi
 
-# If $RECYCLE_DIR doesn't exists, create it.
-[ ! -d "$RECYCLE_DIR" ] \
-    && mkdir -p "$RECYCLE_DIR"
+# If $recycle_dir doesn't exists, create it.
+[ ! -d "$recycle_dir" ] \
+    && mkdir -p "$recycle_dir"
 
 # Start main program loop. Loops through all args given.
-for ITEM in "$@"; do
+for item in "$@"; do
     # If item exists, continute.
-    if [ -e "$ITEM" ]; then
+    if [ -e "$item" ]; then
         # If link, remove.
-        if [ -L "$ITEM" ]; then
+        if [ -L "$item" ]; then
             {
-                /usr/bin/rm -rf "$ITEM" \
-                && printf "Removed link \"%s\"\n." "$ITEM"
+                /usr/bin/rm -rf "$item" \
+                && printf "Removed link \"%s\"\n." "$item"
             } || {
-                /usr/bin/sudo /usr/bin/rm -rf "$ITEM" \
-                && printf "Removed link \"%s\" using sudo.\n" "$ITEM"
+                /usr/bin/sudo /usr/bin/rm -rf "$item" \
+                && printf "Removed link \"%s\" using sudo.\n" "$item"
             }
         # If item empty, remove.
-        elif [ ! -s "$ITEM" ]; then
+        elif [ ! -s "$item" ]; then
             {
-                /usr/bin/rm -rf "$ITEM" \
-                && printf "Removed empty file \"%s\".\n" "$ITEM"
+                /usr/bin/rm -rf "$item" \
+                && printf "Removed empty file \"%s\".\n" "$item"
             } || {
-                /usr/bin/sudo /usr/bin/rm -rf "$ITEM" \
-                && printf "Removed empty file \"%s\" using sudo.\n" "$ITEM"
+                /usr/bin/sudo /usr/bin/rm -rf "$item" \
+                && printf "Removed empty file \"%s\" using sudo.\n" "$item"
             }
         else
             # If item exists in recycle bin, rename until no match.
-            [ -e "$RECYCLE_DIR/$ITEM" ] \
+            [ -e "$recycle_dir/$item" ] \
                 && handle_item_exists
 
             # Move file to trash.
             {
-                /usr/bin/mv "$ITEM" "$RECYCLE_DIR/$ITEM" \
-                && printf "Item \"%s\" to \"%s\"\n" "$ITEM" "$RECYCLE_DIR"
+                /usr/bin/mv "$item" "$recycle_dir/$item" \
+                && printf "Item \"%s\" to \"%s\"\n" "$item" "$recycle_dir"
             } || {
-                /usr/bin/sudo /usr/bin/mv "$ITEM" "$RECYCLE_DIR/$ITEM" \
-                && printf "Item \"%s\" to \"%s\" using sudo\n" "$ITEM" "$RECYCLE_DIR"
+                /usr/bin/sudo /usr/bin/mv "$item" "$recycle_dir/$item" \
+                && printf "Item \"%s\" to \"%s\" using sudo\n" "$item" "$recycle_dir"
             }
         fi
     else
-        printf ":: Item \"%s\" does not exist.\n" "$ITEM"
+        printf ":: Item \"%s\" does not exist.\n" "$item"
         printf "::\n"
         printf ":: Use --help/-h for help.\n"
     fi
